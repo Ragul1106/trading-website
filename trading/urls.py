568d904fb93f883196c46bc5299217ecee7b15a5
Ragul116 <ragulranjith1106@gmail.com>
2025-09-04 21:22:45 +0530

    add blogs

--- trading/urls.py
+++ trading/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -12,4 +14,5 @@ urlpatterns = [
     path('blog/', views.blog, name="blog"),
     
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
