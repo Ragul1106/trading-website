78983e164cd9da7fba22f0aadbcbe2aea3eb1308
Ragul116 <ragulranjith1106@gmail.com>
2025-09-04 19:28:28 +0530

    add blogs

--- trading_project/settings.py
+++ trading_project/settings.py
@@ -13,11 +13,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')  # Load environment variables from .env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
